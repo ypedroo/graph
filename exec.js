6423e77162b7d43afa966d043dd492336b664e06
ypedro <pedromota99@gmail.com>
2017-10-08 18:10:35 -0300

    Exec

--- exec.js
+++ exec.js
@@ -10,4 +10,4 @@ let a1 = graph.addEdge(v4, v1);
 let a2 = graph.addEdge(v2, v4);
 let a3 = graph.addEdge(v3, v3);
 let a4 = graph.addEdge(v3, v4);
-let a5 = graph.addEdge(v5, v3)
+let a5 = graph.addEdge(v5, v3);
